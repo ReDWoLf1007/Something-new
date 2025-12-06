e8dc46ab8ef7b26204aa4a4e2c3c067947e12eba
Sagar Tiwari <sagar.tiwari.work@gmail.com>
2025-12-06 17:22:25 +0530

    Client Improvements 2.5

--- client/src/components/Analytics.jsx
+++ client/src/components/Analytics.jsx
@@ -1,18 +1,20 @@
+import FlipNumbers from 'number-flip';
+
 export const Analytics = () => {
     return (
         <>
             <section className="section-analytics">
           <div className="container grid grid-four-cols">
             <div className="div1">
-              <h2>50+</h2>
+              <h2><FlipNumbers number="50+" /></h2>
               <p>registered companies</p>
             </div>
             <div className="div1">
-              <h2>10,000+</h2>
+              <h2><FlipNumbers number="10,000+" /></h2>
               <p>Happy Clients</p>
             </div>
             <div className="div1">
-              <h2>500+</h2>
+              <h2><FlipNumbers number="500+" /></h2>
               <p>Well known developers</p>
             </div>
             <div className="div1">
