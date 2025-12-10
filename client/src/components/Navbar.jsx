43e4f845f7a16bf0a0cb2700ded974bbe68eddad
Sagar Tiwari <sagar.tiwari.work@gmail.com>
2025-12-10 17:54:40 +0530

    footer check 1.1.3

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export const Navbar = () => {
     const { isLoggedIn } = useAuth();
     return <>
       <header>
-        <div className="container">
+        <div className="navbar-container">
             <div className="logo-brand">
                 <a href="/">Tiwari Logical Analysis</a>
             </div> 
